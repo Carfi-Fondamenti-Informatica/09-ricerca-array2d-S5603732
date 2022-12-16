6a15e84e1348a238d52d0ed5758344f317e78317
S5603732 <118397147+S5603732@users.noreply.github.com>
2022-12-16 15:55:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+
+using namespace std;
 
 int main(){
+   char a[10][20];
+    int i;
+    for(i = 0; i < 10; i++){
+        cin >> a[i];
+    }
+    char b[20];
+    cin >> b;
+    listanomi(a,b);
   return 0;
 }
